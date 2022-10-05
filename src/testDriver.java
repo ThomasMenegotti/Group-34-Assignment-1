de553bc27f441e51a72aadeebc7330ff4e73cfd1
unknown <thomasmenegotti@gmail.com>
2022-10-05 15:45:16 -0400

    Try #2

--- src/testDriver.java
+++ src/testDriver.java
@@ -2,14 +2,14 @@ public class testDriver {
    public static void main(String[] args) {
       // get Product factory
       AbstractFactory ProductFactory = FactoryProducer.getFactory(false);
-      AbstractFactory ProductFactory1 = FactoryProducer.getFactory(true);
+      AbstractFactory PriceFactory = FactoryProducer.getFactory(true);
       Product Product1 = ProductFactory.getProduct("BANANAS");
       Product1.getProduct();
       Product Product3 = ProductFactory1.getProduct("BANANAS");
       Product3.getProduct();
       Product Product2 = ProductFactory.getProduct("APPLES");
       Product2.getProduct();
-      Product Product4 = ProductFactory1.getProduct("APPLES");
+      Product Product4 = PriceFactory.getProduct("APPLES");
       Product4.getProduct();
 
    }
